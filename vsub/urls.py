b07f4997b72702023721786de425533db38b5867
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-25 11:37:16 -0700

    Set the default URL to point to index.html.

--- vsub/urls.py
+++ vsub/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -7,4 +9,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^/?$', TemplateView.as_view(template_name='index.html')),
 )
+
+urlpatterns += staticfiles_urlpatterns()

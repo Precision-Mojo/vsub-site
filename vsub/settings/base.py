a7ac10e912318c0bb5da99ca4262d9bf8b036a91
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-27 14:07:49 -0700

    Add a module for context processors and install a context processor that provides a 'analytical_domain' value for django-analytical.

--- vsub/settings/base.py
+++ vsub/settings/base.py
@@ -134,6 +134,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    '%s.context_processors.analytical_domain' % SITE_NAME,
 )
 
 

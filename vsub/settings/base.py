0cd31713f73aff2f8aeb899b9e85cbaa5d1a46fb
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-26 23:37:45 -0700

    Add myself to the ADMINS setting.

--- vsub/settings/base.py
+++ vsub/settings/base.py
@@ -39,7 +39,7 @@ TEMPLATE_DEBUG = DEBUG
 ## Admin and manager configuration
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ('Your Name', 'your_email@example.com'),
+    ('Marcus R. Brown', 'mrbrown@precision-mojo.com'),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers

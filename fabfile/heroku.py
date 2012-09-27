e395e1d427aee25d25c1b6a72302efeab06e5b2c
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-27 03:39:01 -0700

    Add a fabfile helper to get a config value from Heroku.

--- fabfile/heroku.py
+++ fabfile/heroku.py
@@ -1,6 +1,17 @@
 """Tasks that deal with Heroku."""
 
+import os
 from contextlib import contextmanager
+from fabric.api import hide, local
+
+
+def get_config(key, set_environ=True):
+    with hide('running'):
+        result = local('heroku config:get %s' % key, capture=True).strip()
+    if set_environ:
+        os.environ[key] = result
+    return result
+
 
 @contextmanager
 def maintenance():

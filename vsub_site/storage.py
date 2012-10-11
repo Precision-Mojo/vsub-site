011a3253743ff28a0771b9a1b313660dff33f9f7
Marcus R. Brown <mrbrown@precision-mojo.com>
2012-10-11 13:52:03 -0700

    Try a workaround for django-storage S3BotoStorage class stripping the trailing slash for URLs.

--- vsub_site/storage.py
+++ vsub_site/storage.py
@@ -3,5 +3,18 @@ from pipeline.storage import PipelineMixin
 from storages.backends.s3boto import S3BotoStorage
 
 
-class S3PipelineStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+class UrlCorrectedS3BotoStorage(S3BotoStorage):
+    """
+    Overrides the url() method to readd a trailing slash if it was removed by
+    the base class.
+    """
+
+    def url(self, name):
+        url = super(UrlCorrectedS3BotoStorage, self).url(name)
+        if name.endswith('/') and not url.endswith('/'):
+            url += '/'
+        return url
+
+
+class S3PipelineStorage(PipelineMixin, CachedFilesMixin, UrlCorrectedS3BotoStorage):
     pass

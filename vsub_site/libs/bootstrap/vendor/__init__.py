81b3b54356e15196ba3643c141932c55f6ff3c6d
Marcus R. Brown <mrbrown@precision-mojo.com>
2012-10-11 19:36:07 -0700

    Add the boostrap/vendor module.

---
+++ vsub_site/libs/bootstrap/vendor/__init__.py
@@ -0,0 +1,12 @@
+submodules = {
+    'bootstrap': {
+        'tag': 'v2.1.1',
+        'sources': ['img', 'js', 'less'],
+        'destination': '../static/bootstrap',
+    },
+    'Font-Awesome': {
+        'tag': '2.0.0',
+        'sources': ['font', 'less'],
+        'destination': '../static/bootstrap',
+    },
+}
